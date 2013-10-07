6077c821659141a0e03968bf789d784cbfad09b8
Syrak <li-yao.xia@ens.fr>
2013-10-08 00:00:45 +0200

    Trying tricky implementation

--- solver/opt_naive.c
+++ solver/opt_naive.c
@@ -2,65 +2,98 @@
 #include "target.h"
 #include "opt_naive.h"
 
-Target *buff[BUFF_SIZE];
+typedef struct List List;
 
-num gtime_buff;
+struct TList {
+  int hd_id;
+  num hd_time;
+  TList *tl;
+};
 
-void naive_det_(Target**, int, const int);
+#define BUFF_2SIZE 65536
 
-num naive_det(Target** t, const int n)
+TList tlbuff[BUFF_2SIZE];
+num probabuff[BUFF_2SIZE][BUFF_SIZE];
+num powerbuff[BUFF_2SIZE][BUFF_SIZE];
+num sumbuf[BUFF_SIZE];
+
+void naive_det_(Target*, int*, int, int, const int);
+void update_col(Target, int);
+void reset_list(int);
+
+num naive_det(Target* t, int* permu, const int n)
 {
-  gtime_buff = 1./0.;
+  naive_det_(t, permu, 0, 0, n);
 
-  naive_det_(t, 0, n);
-
-  int i;
-
-  for (i = 0 ; i < n ; i++)
-  {
-    t[i] = buff[i];
-  }
-
-  return gtime_buff;
+  return tlbuff[0].hd_time;
 }
 
-void naive_det_(Target** t, int i, const int n)
+void naive_det_(Target* t, int* permu, int i, int s, const int n)
 {
-  if (i == n)
+  if (i < n)
   {
-    num gtime = guru_time(t, n);
-    
-    if (gtime < gtime_buff)
+    int j;
+    int x = permu[i], id;
+    num time = 1./0;
+
+    for (j = i ; j < n ; j++)
     {
-      gtime_buff = gtime;
+      permu[i] = permu[j];
+      permu[j] = x;
 
-      int j;
+      int s_ = s | 1 << permu[i];
 
-      for (j = 0 ; j < n ; j++)
-      {
-        buff[j] = t[j];
-      }
+      if (tlbuff[s_] == NULL)
+        naive_det_(t, permu, i+1, s_, n);
+
+      num tgt_time = 
+
+      permu[j] = permu[i];
     }
 
+    t[n-1] = t[i];
+    t[i] = tgt;
+  }
+}
+
+void update_col(Target tgt, const int j)
+{
+  if (j == 0)
+  {
+    probabuff[0][0] = 1 - tgt.p;
+    probabuff[1][0] = tgt.p;
+
+    powerbuff[0][0] = tgt.e;
     return;
   }
-
-  int j;
-  Target *tgt = t[i];
-
-  naive_det_(t, i+1, n);
-
-  for (j = i+1 ; j < n ; j++)
+  else
   {
-    t[j-1] = t[i];
-    t[i] = t[j];
-    t[j] = tgt;
+    int i, d = 1 << j, k, l;
+    num sum_ = 0;
 
-    naive_det_(t, i+1, n);
+    powerbuff[0][j] = tgt.e;
+
+    for (i = 1, l = 0, d = 1 ; l < j ; l++, d <<= 1)
+      for (k = 0 ; k < d ; k++)
+        powerbuff[i++][j] = tgt.e + powerbuff[k][l];
+
+    for (i = 0 ; i < d; i++)
+    {
+      probabuff[i][j] = (1 - tgt.p) * probabuff[i][j-1];
+      probabuff[i+d][j] = tgt.p * probabuff[i][j-1];
+
+      sum_ += probabuff[i][j-1] * powerbuff[i][j];
+    }
+
+    sum[j] = tgt.p * sum_ + (1 - tgt.p) * sum[j-1];
   }
+}
 
-  t[n-1] = t[i];
-  t[i] = tgt;
+void reset_list(int p)
+{
+  int i;
+  for (i = 0 ; i < p ; i++)
+    tlbuff[i].hd_id = NULL;
 }
 
 // Quick hack
